df3aac1ad1a83d64a45f1117d8a90e56c4830329
Niten Nashiki <niten.nashiki@lightz-inc.com>
2022-05-01 00:32:04 +0900

    format

--- src/views/components/HelloWorld.vue
+++ src/views/components/HelloWorld.vue
@@ -2,24 +2,20 @@
   <v-container>
     <v-row class="text-center">
       <v-col>
-        <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-        >
+        <v-form ref="form" v-model="valid" lazy-validation>
           <v-text-field
-              v-model="name"
-              :counter="10"
-              :rules="nameRules"
-              label="タスク名"
-              required
+            v-model="name"
+            :counter="10"
+            :rules="nameRules"
+            label="タスク名"
+            required
           ></v-text-field>
 
           <v-btn
-              :disabled="!valid"
-              color="success"
-              class="mr-4"
-              @click="addTask"
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="addTask"
           >
             タスク 追加
           </v-btn>
@@ -28,8 +24,8 @@
       <v-col class="mb-5" cols="12">
         <div v-for="(checkbox, i) in checkboxes" :key="i">
           <v-checkbox
-              v-model="checkbox.status"
-              :label="checkbox.lavel"
+            v-model="checkbox.status"
+            :label="checkbox.lavel"
           ></v-checkbox>
         </div>
       </v-col>
@@ -55,19 +51,19 @@ export default Vue.extend({
       },
     ],
     valid: true,
-    name: '',
+    name: "",
     nameRules: [
-      (v: string) => !!v || 'Name is required',
-      (v: string) => (v && v.length <= 10) || "Name must be less than 10 characters",
+      (v: string) => !!v || "Name is required",
+      (v: string) =>
+        (v && v.length <= 10) || "Name must be less than 10 characters",
     ],
   }),
 
   methods: {
     addTask() {
-      this.checkboxes.push({status: false, lavel: this.name});
-      (this.$refs as any).form.reset()
+      this.checkboxes.push({ status: false, lavel: this.name });
+      (this.$refs as any).form.reset();
     },
   },
-
 });
 </script>
